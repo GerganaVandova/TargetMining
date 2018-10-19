4e58fa4df9523659cf0237c33717c48678eddff6
Gergana Vandova <gvandova@stanford.edu>
2018-10-19 13:01:02 -0700

    Refine the mibig positive dataset to contain only polyketide biosynthetic cluster ids

---
+++ Blast/parse_json.py
@@ -0,0 +1,31 @@
+import json
+import sys
+import glob
+
+# Parse json files and print the genbank ids of only polyketidespython
+# parse_json.py > mibig.gbids.pks
+
+ids_to_class = {}
+ids = []
+
+json_files = glob.glob("mibig_json_1.4/*.json")
+for json_file in json_files:
+    with open(json_file, 'rU') as f:
+        b = json.loads(f.read())
+        # print b
+        p = b['general_params']
+        # print p
+        l = p['loci']
+        bsc = p['biosyn_class'][0]
+        # print bsc
+        gbid = l['nucl_acc'][0]['Accession']
+        # print gbid
+        if bsc == 'Polyketide':
+            ids_to_class[gbid] = bsc
+            ids.append(gbid)
+
+# for key in ids_to_class.keys():
+#     print key
+
+for id in ids:
+    print id
