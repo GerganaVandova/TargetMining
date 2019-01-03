a53034f9bcc2e4251a50f60a18d903f54af78e30
Gergana Vandova <gvandova@stanford.edu>
2019-01-03 11:38:01 -0800

    Display cluster gbid and coord on tree description

--- Phylogeny/get_clustertype.py
+++ Phylogeny/get_clustertype.py
@@ -8,8 +8,10 @@ gbid_to_feat = defaultdict(list)
 
 # Get cluster type and target  from antismash output file
 # AdmT_ACC  ACXX02000001    1   14512-116691    transatpks-nrps Cpap_3701_acetyl-CoA    31720   32586   118.0   304.0   288 0.39    1e-67   38012   39229   5426102179
+
 antismashfilename = "../Antismash_gbids/out.targets.12.eval.1e-8.pident.30.filtered.10000.allpks.domains.268.taxa"
 antismashfile = open(antismashfilename).readlines()[1:]
+
 for line in antismashfile:
     feats = line.split("\t")
     target, gbid, cluster_num, coord, cluster_type = feats[:5]
@@ -18,15 +20,20 @@ for line in antismashfile:
     gbidfull = gbid + "_" + ks_start + "-" + ks_end
     gbid_to_feat[gbidfull] = ((target, cluster_type))
 
-f = open("out.targets.12.eval.1e-8.pident.30.filtered.10000.allpks.domains.268.taxa.cluster_type", "w")
+#f = open("out.targets.12.eval.1e-8.pident.30.filtered.10000.allpks.domains.268.taxa.cluster_type", "w")
+f = open("out.targets.12.eval.1e-8.pident.30.filtered.10000.allpks.domains.268.taxa.descr", "w")
 
 # Read KS fasta file and write cluster type and target for each gbid
 # >ACXX02000001_38012-39229
+
 fasta_file = "out.targets.12.eval.1e-8.pident.30.filtered.10000.allpks.domains.268.taxa.KS.fasta"
+
 for record in SeqIO.parse(open(fasta_file, "rU"), "fasta"):
     gbidfull = record.id
-    print gbidfull, "_".join(gbid_to_feat[gbidfull])
-    f.write("%s\t%s\n" % (gbidfull, "_".join(gbid_to_feat[gbidfull])))
+    feats = gbidfull+ "_" + "_".join(gbid_to_feat[gbidfull])
+    print feats
+    #f.write("%s\t%s\n" % (gbidfull, "_".join(gbid_to_feat[gbidfull])))
+    f.write("%s\t%s\n" % (gbidfull, feats))
 
 f.close()
 
